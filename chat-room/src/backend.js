8347f25cd68fb0dc20c1fa7c4b2930848d542558
arniwattchonkiattipoom <arniwatt.chonkiattipoom@exxonmobil.com>
2021-02-20 11:56:56 +0700

    #5 - Update README

--- chat-room/src/backend.js
+++ chat-room/src/backend.js
@@ -7,6 +7,7 @@ const EventSource = require("eventsource");
 app.use(cors());
 
 app.get('/', (req, res) => {
+    res.send('Hello World!')
     const evtSource = new EventSource("http://localhost:3000/", { withCredentials: true } );
     evtSource.onmessage = function(event) {
         const newElement = document.createElement("li");
@@ -15,7 +16,6 @@ app.get('/', (req, res) => {
         newElement.textContent = "message: " + event.data;
         eventList.appendChild(newElement);
         console.log(eventList)
-        res.send('Hello World!')
     }
 })
 
